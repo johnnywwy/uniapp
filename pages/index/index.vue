6e90e06cc8d7c718d2cb4953ca02cca199ee94c6
johnnywwy <johnnywwy@gmail.com>
2022-08-16 16:47:18 +0800

    引入free和common两个cs库

---
+++ pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="container">
+		<view class="flex align-center justify-center ">
+			<div class='bg-danger w-100'>
+				123
+			</div>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
